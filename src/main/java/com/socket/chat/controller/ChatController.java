abdda64de637196522f5b537acd2f85447166ceb
전승원 <jswa7308@naver.com>
2022-04-20 17:24:27 +0900

    :sparkles: :: 연결 테스트용도 동적 템플릿 작성

---
+++ src/main/java/com/socket/chat/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.socket.chat.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ChatController {
+    @GetMapping("/chat")
+    public String chat(){
+        log.info("@ChatController");
+        return "socket/chat";
+    }
+}
